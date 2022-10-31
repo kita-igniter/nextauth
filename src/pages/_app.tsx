e8f559515bfe41aa90c90ba98dfe6be7eb2cd50f
kita-igniter <smtecsystem.469@gmail.com>
2022-10-31 19:03:18 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AuthProvider } from "../../contexts/AuthContext";
+
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AuthProvider>
+       <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
+
+export default MyApp
