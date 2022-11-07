072fe24bbce9af754da25a01bedc5da04bae1108
kita-igniter <smtecsystem.469@gmail.com>
2022-11-07 07:10:31 -0300

    Token in SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { FormEvent, useContext, useState } from "react";
+import { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
 import { AuthContext } from "../../contexts/AuthContext";
 
 export default function Home() {
@@ -25,4 +27,22 @@ export default function Home() {
       <button type="submit">Entrar</button>
     </form>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies["nextauth.token"]) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      }
+    }
+  }
+  
+  return {
+    props: {}
+  }
 }
