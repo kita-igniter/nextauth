ea61ea57f38baf8ee7d61d6269941cc42efa9ddb
kita-igniter <smtecsystem.469@gmail.com>
2022-11-08 07:22:23 -0300

    Auth with Server

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
-import { api } from "../../services/api";
+import { setupAPIClient } from "../../services/api";
+import { api } from "../../services/apiClient";
+import { withSSRAuth } from "../../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
@@ -14,3 +16,14 @@ export default function Dashboard() {
     <h1>Dashboard: {user?.email}</h1>
   ); 
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const res = await apiClient.get("/me");
+
+  console.log(res.data);
+
+  return {
+    props: {}
+  }
+});
