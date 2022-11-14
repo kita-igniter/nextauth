e1374b6d5a9c2e07795a843898335dc7395d7598
kita-igniter <smtecsystem.469@gmail.com>
2022-11-14 06:35:56 -0300

    Broadcast

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -18,6 +18,7 @@ type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
   isAuthenticated: boolean;
   user: User;
+  signOut: () => void;
 };
 
 type AuthProviderProps = {
@@ -26,9 +27,14 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
-export function signOut() {
+let authChannel: BroadcastChannel;
+
+export function signOut(isBroadcast = true) {
   destroyCookie(undefined, "nextauth.token");
   destroyCookie(undefined, "nextauth.refreshToken");
+
+  isBroadcast && authChannel.postMessage("signOut");
+
   Router.push("/");
 }
 
@@ -36,6 +42,20 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    authChannel = new BroadcastChannel("auth");
+    authChannel.onmessage = (message) => {
+  
+      switch (message.data) {
+        case "signOut":
+          signOut(false);
+          break;
+        default:
+          break;
+      }
+    }
+  }, []);
 
   useEffect(() => {
     const { "nextauth.token": token } = parseCookies();
@@ -83,7 +103,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+    <AuthContext.Provider value={{ signIn,signOut,isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
